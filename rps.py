19a5eb7a75d085115c1b192fa26b1d2b0f62a980
Anany-Gupta <47035529+Anany-Gupta@users.noreply.github.com>
2020-05-08 19:18:03 +0530

    Add files via upload

--- rps.py
+++ rps.py
@@ -16,9 +16,13 @@ ch='y'
 def play(): 
     p1ch=int(input("Player 1 Enter your choice\n  1.Rock\n  2.Paper\n  3.Scissors\n  "))
     p2ch=int(input("Player 2 Enter your choice\n  1.Rock\n  2.Paper\n  3.Scissors\n  "))
-    m=cal(p1ch,p2ch,flag)
-    return m
-def cal(a,b,flag=0):
+    if ((p1ch==1) or (p1ch==2) or (p1ch==3)):
+      if ((p2ch==1) or (p2ch==2) or (p2ch==3)): 
+       m=cal(p1ch,p2ch,-1)
+       return m
+    else:
+       print("please input correct values!!!!!")
+def cal(a,b,flag=-1):
     if (a==b):
         print("Its a TIE")
     elif (a==1):
@@ -27,18 +31,18 @@ def cal(a,b,flag=0):
             flag=1
         else:    
             print(p1," chooose Rock and ",p2," choose Scissors hence",p1,"  WINS!!!")
-            
+            flag=0
     elif (a==2):
         if (b==3):
             print(p1," chooose Paper and ",p2," choose Scissors hence",p2,"  WINS!!!")
             flag=1
         else:    
             print(p1," chooose Paper and ",p2," choose Rock hence",p1,"  WINS!!!")
-            
+            flag=0
     elif (a==3):
         if (b==2):
             print(p1," chooose Scissors and ",p2," choose Paper hence",p1,"  WINS!!!")
-            
+            flag=0
         else:    
             print(p1," chooose Scissors and ",p2," choose Rock hence",p2,"  WINS!!!")        
             flag=1
@@ -47,11 +51,14 @@ def cal(a,b,flag=0):
 while (ch=='y')or(ch=='Y'):
    sp1=0
    sp2=0
+   i=1
    while (sp1<5) & (sp2<5):
+       print("\n\n\n\nRound  ", i," :\n " )
        flag=play()
        if (flag==0):sp1+=1
-       else:sp2+=1     
-       print("Score is ",p1,"a= ",sp1,p2,"  = ",sp2)
+       elif(flag==+1):sp2+=1     
+       print("Score is ",p1,"   ",sp1,p2,"  = ",sp2)
+       i+=1
    
    ch=str(input("Do you wish to play again y/n "))
 print("Thank You for playing")   
